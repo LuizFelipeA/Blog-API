5a3bccee8bf8989bdd99c859f1aa865b441178db
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-05-09 22:48:02 -0300

    Implementing Outcome lib and refacting crud responses

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Ether.Outcomes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
@@ -11,5 +12,32 @@ namespace Blog.Controllers
         {
             return Ok();
         }
+
+        public IActionResult SuccessResponse<T>(int statusCode, T value, string? message = null)
+            => StatusCode(
+                statusCode: statusCode,
+                Outcomes.Success()
+                        .WithValue(value: value)
+                        .WithMessage(message: message));
+
+        public IActionResult SuccessResponse(int statusCode, string message)
+            => StatusCode(
+                statusCode: statusCode,
+                Outcomes.Success()
+                        .WithMessage(message: message));
+
+        public IActionResult FailureResponse(int statusCode, string message)
+            => StatusCode(
+                statusCode: statusCode,
+                Outcomes.Failure()
+                        .WithMessage(message: message)
+                        .WithStatusCode(statusCode: statusCode));
+
+        public IActionResult FailureResponse(int statusCode, IEnumerable<string> messages)
+            => StatusCode(
+                statusCode: statusCode,
+                Outcomes.Failure()
+                        .WithMessage(messages: messages)
+                        .WithStatusCode(statusCode: statusCode));
     }
 }
