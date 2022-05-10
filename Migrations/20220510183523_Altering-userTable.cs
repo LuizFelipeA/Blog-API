dcb702b6b92686f4e7ac2ea45d183e836e8338b5
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-05-10 15:54:03 -0300

    Applying migrations

---
+++ Migrations/20220510183523_Altering-userTable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    public partial class AlteringuserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "User",
+                type: "VARCHAR(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "User",
+                type: "VARCHAR(8000)",
+                maxLength: 8000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(8000)",
+                oldMaxLength: 8000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "User",
+                type: "VARCHAR(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "User",
+                type: "VARCHAR(8000)",
+                maxLength: 8000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(8000)",
+                oldMaxLength: 8000,
+                oldNullable: true);
+        }
+    }
+}
