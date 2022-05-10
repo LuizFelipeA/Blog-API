dc9de00d5b576eb32ea25a3d1d281298e0002e50
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-05-10 11:40:54 -0300

    Updating namespace syntax

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,43 +1,42 @@
 using Ether.Outcomes;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Blog.Controllers
+namespace Blog.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class HomeController : ControllerBase
 {
-    [ApiController]
-    [Route("api/")]
-    public class HomeController : ControllerBase
+    [HttpGet("health-check")]
+    public IActionResult Get()
     {
-        [HttpGet("health-check")]
-        public IActionResult Get()
-        {
-            return Ok();
-        }
-
-        public IActionResult SuccessResponse<T>(int statusCode, T value, string? message = null)
-            => StatusCode(
-                statusCode: statusCode,
-                Outcomes.Success()
-                        .WithValue(value: value)
-                        .WithMessage(message: message));
-
-        public IActionResult SuccessResponse(int statusCode, string message)
-            => StatusCode(
-                statusCode: statusCode,
-                Outcomes.Success()
-                        .WithMessage(message: message));
-
-        public IActionResult FailureResponse(int statusCode, string message)
-            => StatusCode(
-                statusCode: statusCode,
-                Outcomes.Failure()
-                        .WithMessage(message: message)
-                        .WithStatusCode(statusCode: statusCode));
-
-        public IActionResult FailureResponse(int statusCode, IEnumerable<string> messages)
-            => StatusCode(
-                statusCode: statusCode,
-                Outcomes.Failure()
-                        .WithMessage(messages: messages)
-                        .WithStatusCode(statusCode: statusCode));
+        return Ok();
     }
+
+    public IActionResult SuccessResponse<T>(int statusCode, T value, string? message = null)
+        => StatusCode(
+            statusCode: statusCode,
+            Outcomes.Success()
+                    .WithValue(value: value)
+                    .WithMessage(message: message));
+
+    public IActionResult SuccessResponse(int statusCode, string message)
+        => StatusCode(
+            statusCode: statusCode,
+            Outcomes.Success()
+                    .WithMessage(message: message));
+
+    public IActionResult FailureResponse(int statusCode, string message)
+        => StatusCode(
+            statusCode: statusCode,
+            Outcomes.Failure()
+                    .WithMessage(message: message)
+                    .WithStatusCode(statusCode: statusCode));
+
+    public IActionResult FailureResponse(int statusCode, IEnumerable<string> messages)
+        => StatusCode(
+            statusCode: statusCode,
+            Outcomes.Failure()
+                    .WithMessage(messages: messages)
+                    .WithStatusCode(statusCode: statusCode));
 }
--- Dtos/EditorCategoryDto.cs
+++ Dtos/EditorCategoryDto.cs
@@ -1,17 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Blog.Dtos
-{
-    public class EditorCategoryDto
-    {
-        [Required]
-        [StringLength(
-            40,
-            MinimumLength = 3,
-            ErrorMessage = "This field must have between 3 and 40 characters")]
-        public string Name { get; set; }
+namespace Blog.Dtos;
 
-        [Required]
-        public string Slug { get; set; }
-    }
+public class EditorCategoryDto
+{
+    [Required]
+    [StringLength(
+        40,
+        MinimumLength = 3,
+        ErrorMessage = "This field must have between 3 and 40 characters")]
+    public string Name { get; set; }
+
+    [Required]
+    public string Slug { get; set; }
 }
